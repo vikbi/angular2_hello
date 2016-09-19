60323c3c4d28d44a63c1324ef090266940556254
vivek.broctagon <vivek.chaudauri@broctagon.com>
2016-09-19 16:47:07 +0530

    tt

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ System.config({
     //use typescript for compilation
     transpiler: 'typescript',
     //typescript compiler options
+
     typescriptOptions: {
         emitDecoratorMetadata: true
     },
